9c63e3222159adb63a576b84f4691cf033f9e0df
sudoaugustin <sudoaugustin@gmail.com>
2024-08-05 17:14:53 +0630

    add: BROWSER env

--- packages/supex/bin.ts
+++ packages/supex/bin.ts
@@ -55,7 +55,7 @@ $browsers.forEach(async (browser, index) => {
     sourcemap: isBuild ? false : 'inline',
     entryPoints: extensions.script.map(ext => `${paths.app}/**/*.${ext}`), // TODO: Check the project folder to compile only ts,tsx or js,jsx
     ...(isSolid ? { jsx: 'preserve', jsxImportSource: 'solid-js' } : { jsx: 'automatic' }),
-    define: { BROWSER: browser },
+    define: { BROWSER: `${browser}` },
   });
 
   if (isBuild) {
